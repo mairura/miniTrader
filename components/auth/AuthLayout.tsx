d89fda3629a179ed435d2c0ea3d3df829585ba1a
brian <mairurat@gmail.com>
2023-03-16 14:36:28 +0300

    updates

--- components/auth/AuthLayout.tsx
+++ components/auth/AuthLayout.tsx
@@ -1,11 +1,25 @@
 import Head from "next/head";
 import { AuthBody, AuthWrapper } from "../auth/Auth.styles";
+import HashLoader from "react-spinners/HashLoader";
+import { useState, useEffect, CSSProperties } from "react";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "gray",
+};
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+
   return (
     <div>
       <Head>
@@ -13,7 +27,27 @@ export default function AuthLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AuthWrapper>
-        <AuthBody className="auth-form">{children}</AuthBody>
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100vw",
+              height: "100vh",
+            }}
+          >
+            <HashLoader
+              color="#7933ff"
+              cssOverride={override}
+              size={100}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        ) : (
+          <AuthBody className="auth-form">{children}</AuthBody>
+        )}
       </AuthWrapper>
     </div>
   );
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,46 +1,12 @@
 import AuthLayout from "@/components/auth/AuthLayout";
 import LoginForm from "@/components/auth/LoginForm";
-import HashLoader from "react-spinners/HashLoader";
-import { useState, useEffect, CSSProperties } from "react";
-
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "gray",
-};
 
 export default function LoginPage() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100vw",
-            height: "100vh",
-          }}
-        >
-          <HashLoader
-            color="#7933ff"
-            cssOverride={override}
-            size={100}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-        </div>
-      ) : (
-        <AuthLayout>
-          <LoginForm />
-        </AuthLayout>
-      )}
+      <AuthLayout>
+        <LoginForm />
+      </AuthLayout>
     </>
   );
 }
