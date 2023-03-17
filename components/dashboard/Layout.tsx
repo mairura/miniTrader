bbb7a688bf5e9e3e92a198bbf18cacd796d1b660
brian <mairurat@gmail.com>
2023-03-17 20:48:12 +0300

    updates

--- components/dashboard/Layout.tsx
+++ components/dashboard/Layout.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import SideBar from "./SideBar";
 import styled from "styled-components";
+import HashLoader from "react-spinners/HashLoader";
+import { useState, useEffect, CSSProperties } from "react";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "gray",
+};
 
 const Container = styled.div`
   display: grid;
@@ -14,15 +22,46 @@ const Container = styled.div`
 `;
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+
   return (
     <>
-      <Container>
-        <SideBar />
-        <div>
-          <Navbar />
-          {children}
-        </div>
-      </Container>
+      {loading ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100vw",
+              height: "100vh",
+              backgroundColor: " #19123b",
+            }}
+          >
+            <HashLoader
+              color="#7933ff"
+              cssOverride={override}
+              size={100}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <Container>
+            <SideBar />
+            <div>
+              <Navbar />
+              {children}
+            </div>
+          </Container>
+        </>
+      )}
     </>
   );
 };
