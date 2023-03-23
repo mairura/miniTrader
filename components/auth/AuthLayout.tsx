ec19f295f157bb85f960f10dbd13c0f090e5316c
mairura <mairurated@gmail.com>
2023-03-23 19:03:09 +0300

    removed loader on auth pages

--- components/auth/AuthLayout.tsx
+++ components/auth/AuthLayout.tsx
@@ -1,25 +1,12 @@
 import Head from "next/head";
 import { AuthBody, AuthWrapper } from "../auth/Auth.styles";
-import HashLoader from "react-spinners/HashLoader";
-import { useState, useEffect, CSSProperties } from "react";
 
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "gray",
-};
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
-}) {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 3000);
-  }, []);
-
+}) { 
   return (
     <div>
       <Head>
@@ -27,27 +14,9 @@ export default function AuthLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AuthWrapper>
-        {loading ? (
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "100vw",
-              height: "100vh",
-            }}
-          >
-            <HashLoader
-              color="#7933ff"
-              cssOverride={override}
-              size={100}
-              aria-label="Loading Spinner"
-              data-testid="loader"
-            />
-          </div>
-        ) : (
+      
           <AuthBody className="auth-form">{children}</AuthBody>
-        )}
+      
       </AuthWrapper>
     </div>
   );
